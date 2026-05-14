dec0a54025dfbef7794dcd35f51bf44c54a0568e
Carmen Alvarez <carmen@rmen.ca>
2026-05-14 05:09:26 +0200

    Adjust scripts for compiling cobol to wasm.
    * Created `scripts/cobol-wasm.sh` to compile a single cobol file.
    * Adjust `examples/call-cobol-from-js/build.sh` to use this `scripts/cobol-wasm.sh`.

--- examples/call-cobol-from-js/build.sh
+++ examples/call-cobol-from-js/build.sh
@@ -1,19 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-script_dir="$(realpath "$(dirname "${BASH_SOURCE[@]}")")"
-mkdir -p "${script_dir}/build"
-build_dir="/workspace/output/build"
-docker_image="ghcr.io/caarmen/cobol-wasm:latest"
+./scripts/cobol-wasm.sh examples/call-cobol-from-js/AnswerToLife.cob _ANSWER__TO__LIFE,_ANSWER__TO__UNIVERSE
 
-docker run --rm -it \
-  -v "$(pwd)/examples/call-cobol-from-js:/workspace/output" \
-  "${docker_image}" \
-  bash -c "cd ${build_dir} && /opt/gnucobol/bin/cobc -C ../AnswerToLife.cob"
-
-docker run --rm -it \
-  -v "$(pwd)/examples/call-cobol-from-js:/workspace/output" \
-  "${docker_image}" \
-  bash -c "cd ${build_dir} && source /workspace/deps/emsdk/emsdk_env.sh && emcc -o AnswerToLife.js -O3 -s WASM=1 -sEXPORTED_FUNCTIONS='[\"_ANSWER__TO__LIFE\", \"_cob_init\", \"_malloc\", \"_free\"]' -sEXPORTED_RUNTIME_METHODS='[\"cwrap\", \"HEAPU8\", \"UTF8ToString\"]' -I/opt/gnucobol-wasm/include -L/opt/gnucobol-wasm/lib -lcob -L/workspace/deps/gmp/.libs -lgmp /workspace/deps/libdb/build_unix/.libs/libdb-5.3.so AnswerToLife.c"
-
-
+mv AnswerToLife.* examples/call-cobol-from-js/build/
--- scripts/cobol-wasm.sh
+++ scripts/cobol-wasm.sh
@@ -5,11 +5,11 @@ docker_image="ghcr.io/caarmen/cobol-wasm:latest"
 
 # Check if a COBOL file path is provided as an argument
 if [ $# -eq 0 ]; then
-  echo "Usage: $0 <path_to_cobol_file>"
+  echo "Usage: $0 <path_to_cobol_file> [function1,function2,function3]"
   exit 1
 fi
 
-cobol_file="$1"
+cobol_file="$1"; shift
 base_name=$(basename "$cobol_file" .cob)
 
 # Extract the directory of the COBOL file
@@ -27,6 +27,6 @@ docker run --rm -it \
   -v "${tmp_dir}:/workspace/output" \
   -v "${input_dir}:/workspace/input" \
   "${docker_image}" \
-  bash -c "cd /workspace/output && source /workspace/deps/emsdk/emsdk_env.sh && emcc -o ${base_name}.js -O3 -s WASM=1 -sEXPORTED_FUNCTIONS='[\"_ANSWER__TO__LIFE\", \"_cob_init\", \"_malloc\", \"_free\"]' -sEXPORTED_RUNTIME_METHODS='[\"cwrap\", \"HEAPU8\", \"UTF8ToString\"]' -I/opt/gnucobol-wasm/include -L/opt/gnucobol-wasm/lib -lcob -L/workspace/deps/gmp/.libs -lgmp /workspace/deps/libdb/build_unix/.libs/libdb-5.3.so ${base_name}.c"
+  bash -c "cd /workspace/output && source /workspace/deps/emsdk/emsdk_env.sh && emcc -o ${base_name}.js -O3 -s WASM=1 -sEXPORTED_FUNCTIONS='[${*}, \"_cob_init\", \"_malloc\", \"_free\"]' -sEXPORTED_RUNTIME_METHODS='[\"cwrap\", \"HEAPU8\", \"UTF8ToString\"]' -I/opt/gnucobol-wasm/include -L/opt/gnucobol-wasm/lib -lcob -L/workspace/deps/gmp/.libs -lgmp /workspace/deps/libdb/build_unix/.libs/libdb-5.3.so ${base_name}.c"
 
 cp "${tmp_dir}"/*.{js,wasm} .
